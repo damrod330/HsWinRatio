ca45587aacb31c42bf6370229507cb07a4b0318a
Konrad Baczyński <damrod330@gmail.com>
2017-05-04 20:53:02 +0200

    App done ver. Alpha 1.0

--- app/src/main/java/com/a16mb/damrod/hearthstonewinratio/DatabaseHelper.java
+++ app/src/main/java/com/a16mb/damrod/hearthstonewinratio/DatabaseHelper.java
@@ -2,9 +2,13 @@ package com.a16mb.damrod.hearthstonewinratio;
 
 import android.content.ContentValues;
 import android.content.Context;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 import android.util.Log;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by Damrod on 03.05.2017.
@@ -21,25 +25,32 @@ public class DatabaseHelper extends SQLiteOpenHelper {
     public static final String COL_4 ="lost_count";
     public static final String COL_5 ="image_id";
 
-
     public DatabaseHelper(Context context) {
         super(context, DATABASE_NAME, null, 1);
         SQLiteDatabase db = this.getWritableDatabase();
+        onCreate(db);
     }
 
     @Override
     public void onCreate(SQLiteDatabase db) {
-db.execSQL("create table if not exists"+ TABLE_NAME+ "(ID INTEGER PRIMARY KEY AUTOINCREMENT, " +
+
+    db.execSQL("CREATE TABLE IF NOT EXISTS "+ TABLE_NAME+ "(ID INTEGER PRIMARY KEY AUTOINCREMENT, " +
         "DECK_NAME TEXT, VICTORY_COUNT INTEGER, LOST_COUNT INTEGER, IMAGE_ID INTEGER)");
-        Log.v("DB", "Database created");
+        Log.v("DB", " CREATE TABLE IF NOT EXISTS (sql executed)");
     }
 
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
     }
 
-    public boolean insertData(String deckName, int victoryCount, int lostCount, int imageId ){
 
+    public void dropTable() {
+        SQLiteDatabase db = this.getWritableDatabase();
+        db.execSQL("DROP TABLE IF EXISTS '" + TABLE_NAME + "'");
+        Log.v("DB", " DROP TABLE IF EXISTS (sql executed)");
+    }
+
+    public boolean createDeck(String deckName, int victoryCount, int lostCount, int imageId ){
         SQLiteDatabase db = this.getWritableDatabase();
         ContentValues contentValues = new ContentValues();
         contentValues.put(COL_2, deckName);
@@ -52,8 +63,54 @@ db.execSQL("create table if not exists"+ TABLE_NAME+ "(ID INTEGER PRIMARY KEY AU
             return false;
         }
         else {
-            Log.v("DB", "Data inserted");
+            Log.v("DB", "Data inserted for "+ deckName);
             return true;
         }
+    }
+    public boolean removeDeck(String deckName){
+        SQLiteDatabase db = this.getWritableDatabase();
+        long result = db.delete(TABLE_NAME, COL_2 + "=? ", new String[]{deckName});
+        if(result==-1) {
+            Log.v("DB", "COULDN'T DELETE DECK");
+            return false;
+        }
+        else {
+            Log.v("DB", "DECK: "+ deckName+" DELETED");
+            return true;
+        }
+    }
+
+    public boolean updateDeck(String deckName, int victoryCount, int lostCount){
+        SQLiteDatabase db = this.getWritableDatabase();
+        ContentValues contentValues = new ContentValues();
+        contentValues.put(COL_3, victoryCount);
+        contentValues.put(COL_4, lostCount);
+        long result = db.update(TABLE_NAME, contentValues,COL_2 + "=? ", new String[]{deckName} );
+        if(result==-1) {
+            Log.v("DB", "Data not updated");
+            return false;
+        }
+        else {
+            Log.v("DB", "Data updated for "+ deckName);
+            return true;
+        }
+    }
+
+    public List<deck> getDeckList() {
+        String selectQuery = "SELECT  * FROM " + TABLE_NAME;
+        SQLiteDatabase db  = this.getReadableDatabase();
+        Cursor cursor = db.rawQuery(selectQuery, null);
+        List<deck> data = new ArrayList<deck>();
+
+        if (cursor.moveToFirst()) {
+           do{
+
+                deck d = new deck(cursor.getString(1), cursor.getInt(2), cursor.getInt(3), cursor.getInt(4));
+                data.add(0, d);
+            } while (cursor.moveToNext());
+        }
+        cursor.close();
+        db.close();
+        return data;
     }
 }
--- app/src/main/java/com/a16mb/damrod/hearthstonewinratio/MainActivity.java
+++ app/src/main/java/com/a16mb/damrod/hearthstonewinratio/MainActivity.java
@@ -1,18 +1,32 @@
 package com.a16mb.damrod.hearthstonewinratio;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.ContextThemeWrapper;
+import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageButton;
+import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.List;
+import java.util.Random;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
 
 public class MainActivity extends AppCompatActivity {
 
-    @BindView(R.id.btn_add_new)
-    ImageButton btnAddNew;
+    @BindView(R.id.btn_victory)
+    Button btnVictory;
+
+    @BindView(R.id.btn_defeat)
+    Button btnDefeat;
 
     @BindView(R.id.deck_name_text_view)
     TextView deckNameTextView;
@@ -26,24 +40,176 @@ public class MainActivity extends AppCompatActivity {
     @BindView(R.id.win_ratio_text_view)
     TextView winRatioTextView;
 
-    DatabaseHelper myDb;
+    @BindView(R.id.navbar_linear_layout)
+    LinearLayout navbarLinearLayout;
+
+    @BindView(R.id.body_linear_layout)
+    LinearLayout bodyLinearLayout;
+
+    @BindView(R.id.relative_layout)
+    RelativeLayout relativeLayout;
+
+    private DatabaseHelper myDb;
+    private List<deck> deckList;
+    private deck currentDeck;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        //this line is needet in oreder for injection to work
         ButterKnife.bind(this);
 
         myDb = new DatabaseHelper(this);
-        myDb.insertData("freeze mage", 3,2,0);
+        //test only
+        //myDb.removeDeck("test");
+        //myDb.createDeck("Hunter", 0, 0, R.drawable.hunter_4);
+
+        initialSetup();
+
+        deckNameTextView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                new AlertDialog.Builder(MainActivity.this)
+                        .setTitle("Delete deck " + currentDeck.getDeckName())
+                        .setMessage("Are you sure you want to delete this deck?")
+                        .setPositiveButton(android.R.string.yes, new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int which) {
+                                myDb.removeDeck(currentDeck.getDeckName());
+                                initialSetup();
+                            }
+                        })
+                        .setNegativeButton(android.R.string.no, new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int which) {
+                                // do nothing
+                            }
+                        })
+                        .setIcon(R.drawable.icon)
+                        .show();
+            }
+        });
+
+        btnVictory.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                myDb.updateDeck(currentDeck.getDeckName(), currentDeck.getVictoryCount() + 1, currentDeck.getLostCount());
+                currentDeck.setVictoryCount(currentDeck.getVictoryCount() + 1);
+                loadDataToViews(currentDeck);
+            }
+        });
+
+        btnDefeat.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                myDb.updateDeck(currentDeck.getDeckName(), currentDeck.getVictoryCount(), currentDeck.getLostCount() + 1);
+                currentDeck.setLostCount(currentDeck.getLostCount() + 1);
+                loadDataToViews(currentDeck);
+            }
+        });
     }
 
-    void loadDataToViews(deck deck){
+    public void createDeckButtons(List<deck> decksList) {
+        clearNavLinearLayout();
+        for (final deck deck : decksList) {
+            ImageButton btn = new ImageButton((new ContextThemeWrapper(this, R.style.nav_button)), null, 0);
+            btn.setImageDrawable(getDrawable(deck.getIcon_id()));
+            LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(convertPixelsToDp(55), convertPixelsToDp(55));
+            params.setMargins(convertPixelsToDp(4), convertPixelsToDp(4), convertPixelsToDp(4), convertPixelsToDp(4));
+            btn.setLayoutParams(params);
+            btn.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    currentDeck = deck;
+                    loadDataToViews(currentDeck);
+                }
+            });
+            navbarLinearLayout.addView(btn);
+        }
+    }
+
+    private void clearNavLinearLayout() {
+        navbarLinearLayout.removeAllViewsInLayout();
+        ImageButton btn = new ImageButton((new ContextThemeWrapper(this, R.style.nav_button)), null, 0);
+        btn.setImageDrawable(getDrawable(R.drawable.add_new));
+        LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(convertPixelsToDp(55), convertPixelsToDp(55));
+        params.setMargins(convertPixelsToDp(4), convertPixelsToDp(4), convertPixelsToDp(4), convertPixelsToDp(4));
+        btn.setLayoutParams(params);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                final EditText deckNameEt = new EditText(MainActivity.this);
+                deckNameEt.setHint("Deck name");
+                new AlertDialog.Builder(MainActivity.this)
+                        .setTitle("Create new deck ")
+                        .setView(deckNameEt)
+                        .setPositiveButton(android.R.string.yes, new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int which) {
+                                if(deckNameEt.getText().length() >= 4  && deckNameEt.getText().length() <= 22){
+                                    myDb.createDeck(deckNameEt.getText().toString(), 0, 0, getRandomImage());
+                                    initialSetup();
+                                }
+                                else {
+                                    Toast.makeText(MainActivity.this, "invalid deck name", Toast.LENGTH_SHORT).show();
+                                }
+                            }
+                        })
+                        .setNegativeButton(android.R.string.no, new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int which) {
+
+                            }
+                        })
+                        .setIcon(R.drawable.icon)
+                        .show();
+            }
+        });
+        navbarLinearLayout.addView(btn);
+    }
+
+    private int getRandomImage() {
+        int randomId = new Random().nextInt(7);
+        int[] iconsIds ={
+                R.drawable.mage_13,
+                R.drawable.hunter_4,
+                R.drawable.warlock_21,
+                R.drawable.warrior_11,
+                R.drawable.druid_22,
+                R.drawable.priest_12,
+                R.drawable.paladin_10,
+                R.drawable.shaman_5
+        };
+        return iconsIds[randomId];
+    }
+
+    private void loadDataToViews(deck deck) {
         deckNameTextView.setText(deck.getDeckName());
         gamesWonTextView.setText(Integer.toString(deck.getVictoryCount()));
         gamesLostTextView.setText(Integer.toString(deck.getLostCount()));
+        winRatioTextView.setText(calculateWinRatin(deck.getVictoryCount(), deck.getLostCount()));
+    }
 
+    private String calculateWinRatin(int winCount, int lostCount) {
+        if (winCount + lostCount != 0) {
+            String winRatio = Integer.toString((winCount * 100 / (winCount + lostCount)));
+            return winRatio + "%";
+        } else {
+            return "none";
+        }
+    }
+
+    private int convertPixelsToDp(int pixels) {
+        return Math.round(getResources().getDisplayMetrics().density * pixels);
+    }
+
+    private void initialSetup(){
+        deckList = myDb.getDeckList();
+        if (!deckList.isEmpty()) {
+            bodyLinearLayout.setVisibility(View.VISIBLE);
+            currentDeck = deckList.get(0);
+            createDeckButtons(deckList);
+            loadDataToViews(currentDeck);
+        } else {
+            bodyLinearLayout.setVisibility(View.INVISIBLE);
+            clearNavLinearLayout();
+        }
     }
 }
